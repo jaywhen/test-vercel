e10c9bca807743e3d26c7e6ad050bb8e244811cd
jaywhen <jaywhen@qq.com>
2022-05-05 14:56:41 +0800

    test https

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import styles from '../styles/Home.module.css'
 
-export default function Home({str}) {
+export default function Home() {
+  const handleClick = async () => {
+    let data = {
+      name: 'John Doe',
+      age: '30',
+    }
+    let res = await fetch('/api/test', {
+      method: 'POST',
+      body: JSON.stringify(data)
+    }).then(t => t.json());
+    // console.log(res);
+  }
   return (
     <div className={styles.container}>
-      {str}
+      <button onClick={handleClick}>click</button>
     </div>
   )
-}
-
-export const getStaticProps = async () => {
-  let str = process.env.TEST;
-  return {
-    props: {
-      str
-    }
-  }
 }
